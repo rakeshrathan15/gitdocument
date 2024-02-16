93e2118473605c22b7078ef5029927e6ca5d25cc
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-02-16 13:07:49 +0530

    Add student file

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    private String name;
+    private int rollNumber;
+    private char grade;
+
+    // Constructor
+    public Student(String name, int rollNumber, char grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+
+
+
+
+
+
+}
