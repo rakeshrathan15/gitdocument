eabcabca91c088e25da4c698e769a673912febaf
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-02-16 14:57:13 +0530

    In ADD student exam file added

---
+++ src/ExamStudent.java
@@ -0,0 +1,24 @@
+public class ExamStudent {
+
+    private String subject;
+    private int marks;
+
+    public Exam(String subject, int marks) {
+        this.subject = subject;
+        this.marks = marks;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public String teluguSubject(){
+        return teluguSubject;
+    }
+
+
+}
