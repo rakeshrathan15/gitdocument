0ba281594acc710fbc7f38495cd5979bde1c6bfe
rakesh mekala <rakeshyadavmekala15@gmail.com>
2024-02-16 14:52:48 +0530

    In updated student exam file added

---
+++ src/ExamStudent.java
@@ -0,0 +1,20 @@
+public class ExamStudent {
+
+    private String subject;
+    private int marks;
+
+    public Exam(String subject, int marks) {
+        this.subject = subject;
+        this.marks = marks;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+
+}
